f96d5ea8f66acae253a412e6f50b95faef57e24e
pierreGouedard <HX71jMOc1991>
2020-07-15 19:12:48 +0200

    intermediate refactor

--- search_test.sh
+++ search_test.sh
@@ -5,8 +5,9 @@
 declare -a samplingrates=("sampling_rate=0.1" "sampling_rate=0.4" "sampling_rate=0.8")
 declare -a min_prec=("min_precision=0.6" "min_precision=0.75" "min_precision=0.85")
 
-export PYTHONPATH="$HOME/yala"
-export PYTHONPATH="$HOME/lib:$PYTHONPATH"
+export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/yala"
+export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/lib:$PYTHONPATH"
+
 echo "$PWD"
 
 for sr in "${samplingrates[@]}"
