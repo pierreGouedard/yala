6045e0f671265cb2eec54205e43835e100426cc3
pierreGouedard <HX71jMOc1991>
2020-07-15 19:23:57 +0200

    intermediate refactor

--- scripts/higgs_search/search_3.sh
+++ scripts/higgs_search/search_3.sh
@@ -4,9 +4,9 @@
 declare -a samplingrates=("sampling_rate=0.1" "sampling_rate=0.4" "sampling_rate=0.8")
 declare -a min_prec=("min_precision=0.6" "min_precision=0.75" "min_precision=0.85")
 
-conda activate yala-env && \
-export PYTHONPATH="$HOME/yala" && \
-export PYTHONPATH="$HOME/lib:$PYTHONPATH"
+source $(conda info --base)/etc/profile.d/conda.sh  && conda activate yala-env
+export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/yala"
+export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/lib:$PYTHONPATH"
 
 for sr in "${samplingrates[@]}"
 do
