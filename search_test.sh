3d24588d14bd55e99eb8f04d0f104dc85fb98cc5
pierreGouedard <HX71jMOc1991>
2020-07-15 19:15:03 +0200

    intermediate refactor

--- search_test.sh
+++ search_test.sh
@@ -5,6 +5,7 @@
 declare -a samplingrates=("sampling_rate=0.1" "sampling_rate=0.4" "sampling_rate=0.8")
 declare -a min_prec=("min_precision=0.6" "min_precision=0.75" "min_precision=0.85")
 
+conda init && conda activate yala-env
 export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/yala"
 export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/lib:$PYTHONPATH"
 
