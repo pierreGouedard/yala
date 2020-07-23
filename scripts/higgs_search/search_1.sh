cac94599245a6685fcb33c0caaefb412b5fbcacb
pierreGouedard <HX71jMOc1991>
2020-07-23 18:57:26 +0200

    update search

--- scripts/higgs_search/search_1.sh
+++ scripts/higgs_search/search_1.sh
@@ -2,8 +2,8 @@
 # sbatch --time=100:00:00 --mem-per-cpu=8000M --account=def-lwlcmyky-ab scripts/higgs_search/search_1.sh
 
 # Declare variables
-declare -a samplingrates=("sampling_rate=0.1" "sampling_rate=0.4" "sampling_rate=0.8")
-declare -a min_prec=("min_precision=0.6" "min_precision=0.75" "min_precision=0.85")
+declare -a samplingrates=("sampling_rate=1.0" "sampling_rate=0.6" "sampling_rate=0.8")
+declare -a min_prec=("min_precision=0.8" "min_precision=0.9")
 
 source $(conda info --base)/etc/profile.d/conda.sh  && conda activate yala-env
 export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/yala"
@@ -13,6 +13,6 @@ for sr in "${samplingrates[@]}"
 do
   for mp in "${min_prec[@]}"
   do
-    python DEV/higgs/fitting/yala_search.py -p "$sr" min_firing=100 "$mp"
+    python DEV/higgs/fitting/yala_search.py -p "$sr" min_firing=200 "$mp"
   done
 done
--- scripts/higgs_search/search_2.sh
+++ scripts/higgs_search/search_2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Declare variables
-declare -a samplingrates=("sampling_rate=0.1" "sampling_rate=0.4" "sampling_rate=0.8")
-declare -a min_prec=("min_precision=0.6" "min_precision=0.75" "min_precision=0.85")
+declare -a samplingrates=("sampling_rate=1.0" "sampling_rate=0.6" "sampling_rate=0.8")
+declare -a min_prec=("min_precision=0.8" "min_precision=0.9")
 
 source $(conda info --base)/etc/profile.d/conda.sh  && conda activate yala-env
 export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/yala"
@@ -12,6 +12,6 @@ for sr in "${samplingrates[@]}"
 do
   for mp in "${min_prec[@]}"
   do
-    python DEV/higgs/fitting/yala_search.py -p "$sr" min_firing=1000 "$mp"
+    python DEV/higgs/fitting/yala_search.py -p "$sr" min_firing=500 "$mp"
   done
 done
