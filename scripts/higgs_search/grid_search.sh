f6f8485290ea9edbdf7e221ed84635f624e7ec04
pierreGouedard <HX71jMOc1991>
2020-10-30 18:35:47 +0100

    update grid search script

--- scripts/higgs_search/grid_search.sh
+++ scripts/higgs_search/grid_search.sh
@@ -3,9 +3,9 @@
 
 # Declare variables
 declare -a args1=("min_precision=0.7", "min_precision=0.8", "min_precision=0.9")
-declare -a args1=("min_firing=50", "min_firing=100", "min_firing=200" )
-declare -a args1=("n_overlap=20", "n_overlap=40", "n_overlap=60" )
-declare -a args1=("sampling_rate=0.2", "sampling_rate=0.5", "sampling_rate=0.8" )
+declare -a args2=("min_firing=50", "min_firing=100", "min_firing=200" )
+declare -a args3=("n_overlap=20", "n_overlap=40", "n_overlap=60" )
+declare -a args4=("sampling_rate=0.2", "sampling_rate=0.5", "sampling_rate=0.8" )
 
 source $(conda info --base)/etc/profile.d/conda.sh  && conda activate yala-env
 export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/yala"
@@ -13,5 +13,14 @@ export PYTHONPATH="$HOME/projects/def-lwlcmyky-ab/pvanleeu/lib:$PYTHONPATH"
 
 for arg1 in "${args1[@]}"
 do
-    python DEV/higgs/fitting/yala_search.py -p "$arg1"
+  for arg2 in "${args2[@]}"
+    do
+      for arg3 in "${args3[@]}"
+        do
+          for arg4 in "${args4[@]}"
+            do
+              python DEV/higgs/fitting/yala_search.py -p "$arg1" "$arg2" "$arg3" "$arg4"
+            done
+        done
+    done
 done
