d9340c6ad4df51b4362962394f4e1ae103ed605d
pierre gouedard <pierre.mgouedard@gmail.com>
2022-05-03 19:15:59 -0400

    refactor

--- src/model/core/drainers/cleaner.py
+++ src/model/core/drainers/cleaner.py
@@ -1,11 +1,9 @@
 # Global import
 import numpy as np
 from scipy.sparse import csc_matrix
-from copy import deepcopy as copy
 
 # Local import
 from src.model.utils import sample_from_mask
-from src.model.core.patterns import YalaBasePatterns
 from src.model.core.data_models import FgComponents
 from src.model.core.drainers.visualizer import Visualizer
 
@@ -13,21 +11,20 @@ from src.model.core.drainers.visualizer import Visualizer
 class Cleaner(Visualizer):
     """Expander"""
     def __init__(
-            self, server, sax_bf_map, drainer_params, min_firing=100, n_min_bounds=3, perf_plotter=None,
+            self, server, bitmap, drainer_params, min_firing=100, min_bounds=2, perf_plotter=None,
     ):
         # call parent constructor
-        self.n_min_bounds = n_min_bounds
-        self.ax_cleaned_features = None
-        super().__init__(server, sax_bf_map, drainer_params, min_firing, level_delta=1, perf_plotter=perf_plotter)
+        super().__init__(server, bitmap, drainer_params, min_firing, min_bounds, perf_plotter=perf_plotter)
 
     def select_support_bits(self, sax_drained_weights, sax_count_activations):
+        # TODO: obsolete => Good but meant to remove 1 bound at a time
         # Get selected bits keep if support
         sax_active_inputs = self.select_inputs(sax_drained_weights, sax_count_activations)
         sax_drained_inputs = sax_count_activations > 0
 
         # Compute input cardinality for each features
-        ax_card_selected: np.ndarray = self.b2f(sax_active_inputs.astype(int)).A
-        ax_card_signal: np.ndarray = self.b2f(sax_drained_inputs.astype(int)).A
+        ax_card_selected: np.ndarray = self.bitmap.b2f(sax_active_inputs.astype(int)).A
+        ax_card_signal: np.ndarray = self.bitmap.b2f(sax_drained_inputs.astype(int)).A
 
         # Identify bounds to remove
         ax_mask_selected = ax_card_selected < ax_card_signal
@@ -38,56 +35,21 @@ class Cleaner(Visualizer):
 
         # Keep a minimum number of bounds
         ax_sampled = sample_from_mask(
-            self.b2f(self.fg_mask.I).A & (~ax_mask_selected),
-            ax_n=np.maximum(self.n_min_bounds - ax_mask_selected.sum(axis=1), 0)
+            self.bitmap.b2f(self.fg_mask.I).A & (~ax_mask_selected),
+            ax_n=np.maximum(self.min_bounds - ax_mask_selected.sum(axis=1), 0)
         )
         ax_mask_selected |= ax_sampled
 
         # Build support inputs
-        sax_support_bits = (sax_active_inputs + self.pre_draining_inputs).multiply(
-            self.f2b(csc_matrix(ax_mask_selected.T))
-        ) + self.pre_draining_inputs
+        sax_support_bits = (sax_active_inputs + self.original_inputs).multiply(
+            self.bitmap.f2b(csc_matrix(ax_mask_selected.T))
+        ) + self.original_inputs
 
         # Keep only selected bounds
-        sax_support_bits = sax_support_bits.multiply(self.f2b(csc_matrix(ax_mask_selected.T)))
+        sax_support_bits = sax_support_bits.multiply(self.bitmap.f2b(csc_matrix(ax_mask_selected.T)))
 
-        # Track cleaned features
-        self.ax_cleaned_features = self.b2f(self.fg_mask.I).A.T ^ self.b2f(sax_support_bits).A.T
-        import IPython
-        IPython.embed()
         return sax_support_bits
 
     def reset(self):
-        self.ax_cleaned_features = None
         Visualizer.reset(self)
-
-    def build_patterns(self, component, **kwargs):
-        # Build convex hull
-        sax_hull_inputs = self.build_convex_hull(component, kwargs['x'], kwargs['fg'])
-
-        # Build components
-        fg_comp, mask_comp = self.build_components(component, sax_hull_inputs)
-        self.pre_draining_inputs = component.inputs
-
-        # Create mask pattern from comp
-        self.fg_mask = YalaBasePatterns.from_fg_comp(mask_comp)
-        self.firing_graph = YalaBasePatterns.from_fg_comp(fg_comp)
-
-    def build_components(self, comp, sax_hull_inputs):
-        fg_comp = FgComponents(
-            inputs=csc_matrix(comp.inputs.A ^ self.f2b(self.b2f(comp.inputs).T).A).multiply(sax_hull_inputs),
-            partitions=copy(comp.partitions), levels=np.ones(len(comp))
-        )
-
-        return fg_comp, comp.copy(levels=copy(comp.levels) - 1)
-
-    def build_convex_hull(self, component, sax_x, sax_fg):
-        # Get masked activations
-        sax_product = sax_x.astype(bool).T.dot(sax_fg)
-
-        # Compute feature masks
-        ax_card, ax_mask_component = self.b2f(sax_product.astype(int)).A, self.b2f(component.inputs.astype(bool)).A
-        ax_mask = (0 < ax_card) & (ax_card < self.get_card_features(ax_card.shape[0])) & ax_mask_component
-
-        return sax_product.multiply(self.f2b(csc_matrix(ax_mask.T)))
 
--- src/model/yala.py
+++ src/model/yala.py
@@ -1,12 +1,12 @@
 # Global import
+from numpy import ones
 
 # Local import
-from src.model.core.patterns import YalaBasePatterns
+from src.model.core.firing_graph import YalaFiringGraph
 from src.model.core.server import YalaUnclassifiedServer, YalaMisclassifiedServer
-from src.model.core.data_models import DrainerParameters, TrackerParameters, FgComponents
+from src.model.core.data_models import DrainerParameters, TrackerParameters, FgComponents, ConvexHullProba, BitMap
 from src.model.utils import init_sample
 from src.model.core.encoder import MultiEncoders
-from src.model.core.drainers.cleaner import Cleaner
 from src.model.core.drainers.shaper import Shaper
 from src.model.core.tracker import Tracker
 
@@ -30,7 +30,7 @@ class Yala(object):
                  n_bin=10,
                  bin_method='quantile',
                  bin_missing=False,
-    ):
+                 ):
         # Core parameters
         self.n_run = n_run
         self.max_iter = max_iter
@@ -52,7 +52,7 @@ class Yala(object):
         self.tracker_params = TrackerParameters(min_delta_area=min_delta_area, max_no_changes=max_no_changes)
 
         # Yala Core attributes
-        self.firing_graph = None
+        self.firing_graph, self.bitmap = None, None
 
     def fit(self, X, y, **kwargs):
         """
@@ -64,6 +64,7 @@ class Yala(object):
         """
         # encode: X is a numpy/scipy array, y in numpy/scipy array
         X_enc, y_enc = self.encoder.fit_transform(X=X, y=y)
+        self.bitmap = BitMap(self.encoder.bf_map, self.encoder.bf_map.shape[0], self.encoder.bf_map.shape[1])
 
         # Instantiate core components
         if self.server_type == 'unclassified':
@@ -75,65 +76,94 @@ class Yala(object):
         else:
             raise NotImplementedError
 
-        shaper = Shaper(
-            self.server, self.encoder.bf_map, self.drainer_params, min_firing=self.min_firing,
+        drainer = Shaper(
+            self.server, self.bitmap, self.drainer_params, min_firing=self.min_firing,
             perf_plotter=kwargs.get('perf_plotter', None)
         )
 
-        cleaner = Cleaner(
-            self.server, self.encoder.bf_map, self.drainer_params, min_firing=self.min_firing,
-            perf_plotter=kwargs.get('perf_plotter', None)
-        )
-        # TODO (multi class): should be ok, just implement changes in drainer.setup_params
+        ## TODO: Things to do for this new version:
+        #   * In step 1: CH as mask (full convex level) / shrinked base (level=1) drained:
+        #       * Only bits of base bound between CH of mask & passed base are drained (=> CH computation needed)
+        #       * The integrity & convexity of base should always be preserved after rule applied:
+        #         * if a base bound is not support => remove bound from base
+        #         * if none base bounds are support => keep the entire CH as base
+        #         * if only (n-1) base is support => keep the (n-1) + 1 random (n=2 in 2D space, 3 in 3D, ...)
+        #         * drained base & CH are outputed separately
+        #   * In step 2: Remaining base (full convex level) is mask / CH (level=1) is drained:
+        #       * Same rule of selection applies (except the rule on n-1 support)
+        #       * support of drained CH & mask are merged to compose a new convex base.
+        #   * Track convergence of nodes and iterates if any vertex that has note converged remains.
+
+        n_ch_candidate = 2
         for i in range(self.n_run):
             print("[YALA]: Iteration {}".format(i))
 
             # Initial sampling
-            components, shaper_probas = init_sample(
-                self.n_parallel, self.init_level, self.server, self.encoder.bf_map, window_length=7
+            base_components = init_sample(
+                self.n_parallel, self.init_level, self.server, self.bitmap, window_length=7
             )
+            ch_probas = ConvexHullProba()
 
             # Instantiate tracking
             tracker = Tracker(
-                [d['id'] for d in components.partitions], tracker_params=self.tracker_params,
+                [d['id'] for d in base_components.partitions], tracker_params=self.tracker_params,
                 n_features=self.encoder.bf_map.shape[1]
             )
 
             # Core loop
             i = 0
-            while len(components) > 0 and i < self.max_iter:
+            while len(base_components) > 0 and i < self.max_iter:
 
-                # Alter shape
-                components = shaper.prepare(components, shaper_probas=shaper_probas).drain_all().select()
-                shaper.reset()
+                # Sample bounds for convex hull & update ch proba
+                ch_components = YalaFiringGraph.from_fg_comp(base_components)\
+                    .get_convex_hull(self.server, self.drainer_params.batch_size)
 
-                # Clean shape
-                components = cleaner.prepare(components).drain_all().select()
-                shaper_probas.add(cleaner.ax_cleaned_features).set_probas(cleaner.b2f(components.inputs).A.T)
-                cleaner.reset()
+                ch_components = ch_components.sample(
+                    ch_probas.get_probas(base_components, self.bitmap), n_ch_candidate,
+                    {"levels": ones(len(ch_components)) * n_ch_candidate}
+                )
+                ch_probas.add(ch_components, self.bitmap.b2f(ch_components.inputs.astype(bool)))
 
-                # Swap from compression to expansion & track metrics
-                components = tracker.swap_components(components)
+                # Draining part 1: keep separated components
+                base_components, ch_components = drainer.prepare(
+                    base_components.shrink(self.bitmap, **{'levels': ones(len(base_components))}),
+                    ch_components
+                )\
+                    .drain_all()\
+                    .select(merge=False)
+                drainer.reset()
+
+                # Draining part 2: Merge components
+                base_components = drainer.prepare(
+                    ch_components.update(**{'levels': ones(len(ch_components))}),
+                    base_components
+                )\
+                    .drain_all()\
+                    .select(merge=True)
+                drainer.reset()
+
+                # Swap from compression to expansion & track metrics TODO: ?? rename da shit
+                base_components = tracker.swap_components(base_components)
 
                 if i % 5 == 0:
                     tracker.visualize_indicators()
-                    if components is not None:
-                        cleaner.visualize_fg(YalaBasePatterns.from_fg_comp(components))
+                    if base_components is not None:
+                        drainer.visualize_fg(YalaFiringGraph.from_fg_comp(base_components))
                     resp = input('would you like to activate visualisation now ?')
 
                     if resp == "yes":
-                        shaper.plot_perf_enabled = True
-                        shaper.advanced_plot_perf_enabled = True
-                        cleaner.plot_perf_enabled = True
-                        cleaner.advanced_plot_perf_enabled = True
+                        drainer.plot_perf_enabled = True
+                        drainer.advanced_plot_perf_enabled = True
+                        drainer.plot_perf_enabled = True
+                        drainer.advanced_plot_perf_enabled = True
                 i += 1
 
-                if components is None:
+                if base_components is None:
                     break
 
             tracker.visualize_indicators()
-            completes = tracker.components
-            for comp in completes:
-                cleaner.visualize_fg(YalaBasePatterns.from_fg_comp(comp))
+            complete_components = tracker.components
+            for comp in complete_components:
+                drainer.visualize_fg(YalaFiringGraph.from_fg_comp(comp))
 
         return self
